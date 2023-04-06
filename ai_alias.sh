503528311dc8613b1ef210b156e088859d235fd8
Andrey Kozhuhov <kozhuhov.andrey@gmail.com>
2023-04-06 23:51:46 +0600

    ai_alias.sh update

--- ai_alias.sh
+++ ai_alias.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Check if the alias already exists in the ~/.bash_profile
-if ! grep -q "alias aii=" "$HOME/.bash_profile"; then
-  # Add the alias to the ~/.bash_profile
-	dir="$(pwd)"
+# Check if the "ai" alias already exists in the ~/.bash_profile
+if ! grep -q "alias ai=" "$HOME/.bash_profile"; then
+  # Add the "ai" alias to the ~/.bash_profile
+  dir="$(pwd)"
+  # Create the alias, linking it to the main.py script in the current directory using Python3
   echo "alias ai='python3 $dir/main.py'" >> "$HOME/.bash_profile"
+  # Notify the user that the alias 'ai' was added successfully
   echo "Alias 'ai' added successfully."
 else
+  # Notify the user that the alias 'ai' already exists
   echo "Alias 'ai' already exists."
 fi
 
-# Source the updated ~/.bash_profile
+# Source the updated ~/.bash_profile to make the new alias available immediately
 source "$HOME/.bash_profile"
